6268deafbb0fa0adcb7935c7ffbc5e9db398eb29
mircea32000 <36380975+mircea32000@users.noreply.github.com>
2019-10-17 19:21:57 +0300

    oops

--- CSGOSimple/valve_sdk/interfaces/IVDebugOverlay.hpp
+++ CSGOSimple/valve_sdk/interfaces/IVDebugOverlay.hpp
@@ -33,4 +33,16 @@ public:
     virtual void            AddBoxOverlay2(const Vector& origin, const Vector& mins, const Vector& max, QAngle const& orientation, const uint8_t* faceColor, const uint8_t* edgeColor, float duration) = 0;
     virtual void            PurgeTextOverlays() = 0;
     virtual void            DrawPill(const Vector& mins, const Vector& max, float& diameter, int r, int g, int b, int a, float duration, int unk1, int unk2) = 0;
+
+	inline void DrawPillBullshit(const Vector& mins, const Vector& max, const float& diameter, int r, int g, int b, int a, float duration, int unk1, int unk2)
+	{
+    	typedef void(__thiscall* oCalFrumos)(void*, const Vector*, const Vector*, const float*, int, int, int, int, float, int, int);
+		return CallVFunction<oCalFrumos>(this, 23)(this, &mins, &max, &diameter, r , g , b , a, duration, unk1, unk2);
+	}
+
+	inline void AddBoxBullshit(const Vector& origin, const Vector& mins, const Vector& max, const QAngle& orientation, int r, int g, int b, int a, float duration)
+	{
+		typedef void(__thiscall* oCalFrumos)(void*, const Vector*, const Vector* , const Vector* , const QAngle* , int, int, int , int , float );
+		return CallVFunction<oCalFrumos>(this, 1)(this, &origin, &mins, &max, &orientation, r, g, b, a, duration);
+	}
 };
